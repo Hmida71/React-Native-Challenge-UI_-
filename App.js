ad08b162b3b0e0ede9b304d18992e9b324d8d50e
Hmida <69757558+Hmida71@users.noreply.github.com>
2021-10-01 06:56:42 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import DrawerNavigator from "./navigation/DrawerNavigator";
+
+ const App = () => {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
+export default App
