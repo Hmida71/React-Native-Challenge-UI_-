15e622b6dd4cd2140c99b149120d0ec81523f992
Hmida <69757558+Hmida71@users.noreply.github.com>
2021-10-01 06:59:58 +0100

    Create option_2.js

---
+++ src/src_drawer/option_2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import {styles} from '../Styles/GlobalStyles'
+
+const option_2 = () => {
+  return (
+    <View style={styles.option_2}>
+      <Text>This is the option_2 </Text>
+    </View>
+  );
+};
+
+
+export default option_2;
